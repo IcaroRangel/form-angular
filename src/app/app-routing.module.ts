24d3776f59e5523c170433edef4134a5fe9bbd58
Icaro <icarorangel05@gmail.com>
2022-02-07 15:09:54 -0300

    create components home, data-form, template-form and create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataFormComponent } from './components/data-form/data-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'templateForm',
+    component: TemplateFormComponent,
+  },
+  {
+    path: 'dataForm',
+    component: DataFormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataFormComponent } from './components/data-form/data-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TemplateFormComponent,
+    DataFormComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

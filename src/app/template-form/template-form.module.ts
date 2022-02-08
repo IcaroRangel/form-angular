9839eb897a722643411dc05b285ca3040bedd3af
Icaro <icarorangel05@gmail.com>
2022-02-08 16:18:08 -0300

    add component form-debug

--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from '../components/template-form/template-form.component';
 import { FormsModule } from '@angular/forms';
+import { FormDebugComponent } from '../components/form-debug/form-debug.component';
 
 @NgModule({
-  declarations: [TemplateFormComponent],
+  declarations: [TemplateFormComponent, FormDebugComponent],
   imports: [CommonModule, FormsModule],
 })
 export class TemplateFormModule {}
